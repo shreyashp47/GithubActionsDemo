4e880c891c36d251c1ad5144256054b0183fca27
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-08-10 18:37:16 +0530

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.shreyash.github.actions"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
